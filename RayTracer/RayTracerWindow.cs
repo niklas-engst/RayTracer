37805aefc6ee4899b195a0937b5e300c1bb31834
Niklas Engst <187411134+niklas-engst@users.noreply.github.com>
2026-08-28 10:43:44 +0200

    feat: allow rendering of multiple objects

--- RayTracer/RayTracerWindow.cs
+++ RayTracer/RayTracerWindow.cs
@@ -9,7 +9,8 @@ public class RayTracerWindow(int width, int height) : PixelBufferWindow(width, h
     protected bool SinglePass = true;
 
     protected Vector3d LightPosition =  new Vector3d(0, 2, -1);
-    protected Vector3d SpherePosition = new  Vector3d(0, 0, 1);
+    protected Vector3d SpherePosition = new Vector3d(0, 0, 1);
+    protected Vector3d Sphere2Position = new Vector3d(1, 0, 3);
 
     protected float Specular = 1.0f;
     protected float Shininess = 100.0f;
@@ -48,6 +49,11 @@ public class RayTracerWindow(int width, int height) : PixelBufferWindow(width, h
                 ImGui.SliderFloat3("Sphere position", (float*)spherePosPtr, -10, 10);
             }
             
+            fixed (Vector3d* sphere2PosPtr = &Sphere2Position)
+            {
+                ImGui.SliderFloat3("Sphere2 position", (float*)sphere2PosPtr, -10, 10);
+            }
+            
             ImGui.SliderFloat("Specular", ref Specular, 0.0f, 5.0f);
             ImGui.SliderFloat("Shininess", ref Shininess, 0.0f, 200.0f);
         }
@@ -73,6 +79,8 @@ public class RayTracerWindow(int width, int height) : PixelBufferWindow(width, h
         var material = new CheckeredMaterial(new Color(0, 255, 0, 255), new Color(255, 0, 255, 255), Specular, Shininess, 16, 8);
         var sphere = new Sphere(SpherePosition, 1f, material);
         var light = new Light(LightPosition);
+
+        List<Sphere> objects = [sphere, new Sphere(Sphere2Position, 1.5f, new Material(new Color(255, 0, 0, 255), Specular, Shininess))];
         
         for (var windowY = 0; windowY < RenderHeight; windowY++)
         {
@@ -87,19 +95,32 @@ public class RayTracerWindow(int width, int height) : PixelBufferWindow(width, h
                 var rayDirection = (screenPoint - camera).Normalized();
                 var ray = new Ray(camera, rayDirection);
 
-                var intersection = sphere.Intersection(ray);
-
-                if (!float.IsInfinity(intersection))
-                {
-                    var intersectionPoint = ray.At(intersection);
-                    var color = sphere.Shading(intersectionPoint, light, ray.Direction);
-                    Pixels[windowY * RenderWidth + windowX] = color.ToUInt32();
-                }
-                else
-                {
-                    Pixels[windowY * RenderWidth + windowX] = new Color(128, 128, 128, 128).ToUInt32();
-                }
+                var color = Raytrace(ray, objects, light);
+                
+                Pixels[windowY * RenderWidth + windowX] = color.ToUInt32();
             }
         }
+    }
+    
+    protected Color Raytrace(Ray ray, List<Sphere> objects, Light light)
+    {
+        var closestIntersection = float.PositiveInfinity;
+        Sphere? closestSphere = null;
+
+        foreach (var sphere in objects)
+        {
+            var intersection = sphere.Intersection(ray);
+            if (intersection < closestIntersection)
+            {
+                closestIntersection = intersection;
+                closestSphere = sphere;
+            }
+        }
+
+        if (closestSphere == null)
+            return new Color(128, 128, 128, 128); // Background color
+        
+        var intersectionPoint = ray.At(closestIntersection);
+        return closestSphere.Value.Shading(intersectionPoint, light, ray.Direction);
     }
 }
